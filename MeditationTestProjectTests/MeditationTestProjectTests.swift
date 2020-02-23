fb4134773f513e8d41261458d0aa62581638413d
Sergey Tszyu <tszyu@dbosoft.com.ua>
2020-02-23 17:35:05 +0200

    Initial Commit

---
+++ MeditationTestProjectTests/MeditationTestProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  MeditationTestProjectTests.swift
+//  MeditationTestProjectTests
+//
+//  Created by Sergey Tszyu on 23.02.2020.
+//  Copyright © 2020 Sergey Tszyu. All rights reserved.
+//
+
+import XCTest
+@testable import MeditationTestProject
+
+class MeditationTestProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
