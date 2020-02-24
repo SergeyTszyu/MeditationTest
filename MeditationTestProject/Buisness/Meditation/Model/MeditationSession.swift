6b950cbb486802e3f3cddd7df0295cb6d74f2be9
Sergey Tszyu <tszyu@dbosoft.com.ua>
2020-02-24 09:40:54 +0200

    first commit

---
+++ MeditationTestProject/Buisness/Meditation/Model/MeditationSession.swift
@@ -0,0 +1,9 @@
+//
+//  MeditationSession.swift
+//  MeditationTestProject
+//
+//  Created by Sergey Tszyu on 23.02.2020.
+//  Copyright © 2020 Sergey Tszyu. All rights reserved.
+//
+
+import Foundation
---
+++ MeditationTestProject/Common/Extensions/UIKit/UIView/UIView+Inspectable.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+Inspectable.swift
+//  MeditationTestProject
+//
+//  Created by Sergey Tszyu on 23.02.2020.
+//  Copyright © 2020 Sergey Tszyu. All rights reserved.
+//
+
+import Foundation
